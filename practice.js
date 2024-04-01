10bc22c2ced4c895d34f252ec18d0d5b31707100
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-04-01 16:25:29 +0530

    mini project completed(DOM Practice)

--- practice.js
+++ practice.js
@@ -1,1 +1,23 @@
-console.log('Hello Aditya');
+///basic DOM practice
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'purple') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'green') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
